676d55f0998327c620cfe6e30ff048ba066b1d5a
victor njuguna <victormoore254@gmail.com>
2018-03-21 10:10:55 +0300

    version 2.0 of js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-var add = function(number1, number2) {
-          return number1 + number2;
-          };
+var add = function (number1, number2) {
+ return number1 + number2; };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number1 = parseInt(prompt("Give me a Number to add for you mate"));
+var number2 = parseInt(prompt("Just one more"));
 
-alert(add(number1, number2);
-    
+var result = add(number1, number2);
+
+alert(result);
