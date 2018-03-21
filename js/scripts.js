229d9d5e6bfb862a155882a849d8120664549f27
victor njuguna <victormoore254@gmail.com>
2018-03-21 10:17:17 +0300

    added subtraction multiplication and division formulaes

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,33 @@ var number2 = parseInt(prompt("Just one more"));
 var result = add(number1, number2);
 
 alert(result);
+
+var subtract = function (number1, number2) {
+ return number1 - number2; };
+
+var number1 = parseInt(prompt("Give me a Number to subtract for you mate"));
+var number2 = parseInt(prompt("Just one more"));
+
+var result = subtract(number1, number2);
+
+alert(result);
+
+var multiply = function (number1, number2) {
+ return number1 * number2; };
+
+var number1 = parseInt(prompt("Give me a Number to multiply for you mate"));
+var number2 = parseInt(prompt("Just one more"));
+
+var result = multiply(number1, number2);
+
+alert(result);
+
+var divide = function (number1, number2) {
+ return number1 / number2; };
+
+var number1 = parseInt(prompt("Give me a Number to divide for you mate"));
+var number2 = parseInt(prompt("Just one more"));
+
+var result = divide(number1, number2);
+
+alert(result);
